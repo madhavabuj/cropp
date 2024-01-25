95c80712134725b268f462503da5bc41b269f931
Madhav Abuj <136056107+madhavabuj@users.noreply.github.com>
2024-01-25 11:50:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def send ():
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, msg.as_string())
         server.quit()
-        return data
+        return data['gm']
        
 def gmail2 (*karg) :
       data=request.get_json()
